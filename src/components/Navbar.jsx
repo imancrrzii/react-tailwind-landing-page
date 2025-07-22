a816dbe0af18802e7fc60921bc0e948997578425
Iman Carrazi Syamsidi <99782815+imancrrzii@users.noreply.github.com>
2025-07-22 20:46:46 +0700

    feat: add mobile menu for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router";
 import { menuItems } from "../utils/data";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdClose, MdKeyboardArrowDown, MdMenu } from "react-icons/md";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -52,7 +52,8 @@ const Navbar = () => {
                             </h3>
                             <div>
                               {section.items.map((item, itemIndex) => (
-                                <Link key={itemIndex}
+                                <Link
+                                  key={itemIndex}
                                   to={`/${key}/${item.name
                                     .toLowerCase()
                                     .replace(" ", "-")}`}
@@ -60,7 +61,12 @@ const Navbar = () => {
                                 >
                                   <div className="px-4">
                                     <p className="text-sm font-medium text-gray-900 flex items-center">
-                                      {item.name} {item.isNew && (<span className="ml-2 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue-800">NEW</span>)}
+                                      {item.name}{" "}
+                                      {item.isNew && (
+                                        <span className="ml-2 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue-800">
+                                          NEW
+                                        </span>
+                                      )}
                                     </p>
                                     <p className="text-sm text-gray-500">
                                       {item.desc}
@@ -74,7 +80,8 @@ const Navbar = () => {
                       ) : (
                         <div className="space-y-1">
                           {menuItems[key].items.map((item, index) => (
-                            <Link key={index}
+                            <Link
+                              key={index}
                               to={`/${key}/${item.name
                                 .toLowerCase()
                                 .replace(" ", "-")}`}
@@ -106,14 +113,142 @@ const Navbar = () => {
           </div>
 
           {/* Auth button */}
-          <div>
-            <Link>Login</Link>
+          <div className="hidden md:flex items-center space-x-4">
+            <Link to="/login" className="hover:text-gray-300 hidden xl:block">
+              Login
+            </Link>
+            <Link to="/contact" className="hover:text-gray-300 hidden xl:block">
+              Contact sales
+            </Link>
+            <Link
+              to="/get-started"
+              className="hover:text-gray-300 bg-blue-600 px-4 py-2 rounded-md hover:bg-blue-700 hidden xl:block"
+            >
+              Get Started - for free
+            </Link>
           </div>
 
           {/* Mobile menu */}
-          <div className="md:hidden">mobile menu</div>
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="inline-flex items-center justify-center p-2 rounded-md hover:text-gray-300 hover:bg-gray-700"
+            >
+              {!isMenuOpen ? (
+                <MdMenu className="block w-6 h-6" />
+              ) : (
+                <MdClose className="block w-6 h-6" />
+              )}
+            </button>
+          </div>
         </div>
       </div>
+
+      {/* mobile menu */}
+      {isMenuOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            {Object.keys(menuItems).map((key) => (
+              <div>
+                <button
+                  onClick={() => toggleDropdown(key)}
+                  className="hover:text-gray-300 px-3 py-2 rounded-md flex items-center"
+                >
+                  {menuItems[key].title}
+                  <MdKeyboardArrowDown
+                    className={`ml-2 h-5 w-5 transition-transform ${
+                      activeDropdown === key ? "rotate-180" : ""
+                    }`}
+                  />
+                </button>
+                {activeDropdown === key && (
+                  <div className="bg-white text-black">
+                    <div className="pl-4">
+                      {key === "platform" ? (
+                        menuItems[key].sections.map((section, index) => (
+                          <div key={index} className="py-2">
+                            <h3 className="text-xs font-semibold text-gray-500 tracking-wider mb-2">
+                              {section.title}
+                            </h3>
+                            <div>
+                              {section.items.map((item, itemIndex) => (
+                                <Link
+                                  key={itemIndex}
+                                  to={`/${key}/${item.name
+                                    .toLowerCase()
+                                    .replace(" ", "-")}`}
+                                  className={`group flex items-start p-2 rounded-lg hover:bg-gray-50 `}
+                                >
+                                  <div className="px-4">
+                                    <p className="text-sm font-medium text-gray-900 flex items-center">
+                                      {item.name}{" "}
+                                      {item.isNew && (
+                                        <span className="ml-2 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue-800">
+                                          NEW
+                                        </span>
+                                      )}
+                                    </p>
+                                  </div>
+                                </Link>
+                              ))}
+                            </div>
+                          </div>
+                        ))
+                      ) : (
+                        <div className="space-y-1">
+                          {menuItems[key].items.map((item, index) => (
+                            <Link
+                              key={index}
+                              to={`/${key}/${item.name
+                                .toLowerCase()
+                                .replace(" ", "-")}`}
+                              className={`group flex items-start p-2 rounded-lg hover:bg-gray-50 `}
+                            >
+                              <div className="px-4">
+                                <p className="text-sm font-medium text-gray-900">
+                                  {item.name}
+                                </p>
+                                <p className="text-sm text-gray-500">
+                                  {item.desc}
+                                </p>
+                              </div>
+                            </Link>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+
+            <Link
+              to={"/enterprise"}
+              className="block px-3 py-2 hover:bg-gray-700"
+            >
+              Enterprise
+            </Link>
+            <Link to={"/pricing"} className="block px-3 py-2 hover:bg-gray-700">
+              Pricing
+            </Link>
+            <Link to={"/login"} className="block px-3 py-2 hover:bg-gray-700">
+              Login
+            </Link>
+            <Link
+              to={"/contact-sales"}
+              className="block px-3 py-2 hover:bg-gray-700"
+            >
+              Contact sales
+            </Link>
+            <Link
+              to={"/get-started"}
+              className="block px-3 py-2 bg-blue-600 hover:bg-blue-700"
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles/global.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/global.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/about" element={<div>about</div>} />
+      <Route path="/platform/*" element={<div>Platform Page</div>} />
+      <Route path="/enterprise" element={<div>Enterprise Page</div>} />
+      <Route path="/pricing" element={<div>Pricing Page</div>} />
+      <Route path="/login" element={<div>Login Page</div>} />
+      <Route path="/contact" element={<div>Contact Sales</div>} />
+      <Route path="/get-started" element={<div>Get Started</div>} />
     </Routes>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
